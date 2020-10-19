c1e8111d9e33341edec6e7533c496127426eeb1e
Mike Williams <mike@doublewide.co.uk>
2020-10-19 13:38:58 +0100

    Add notes

--- src/Scripts/Install-Dev-Certificate.ps1
+++ src/Scripts/Install-Dev-Certificate.ps1
@@ -11,3 +11,7 @@ Write-Host "Importing Certificate " $certificate
 $password = ConvertTo-SecureString -String "Password_123" -Force -AsPlainText
 
 Import-PfxCertificate -FilePath $certificate -CertStoreLocation Cert:\CurrentUser\My -Exportable -Password $password
+
+# You need torce git to add the file
+# CD .\src\Scripts\Certificates\Developer
+# git add -f Chem4WordAddIn.pfx
