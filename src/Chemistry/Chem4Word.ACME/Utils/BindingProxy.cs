3409f69d8fd898caefafd3862a3185fa889f8086
Mike Williams-UK <mike@doublewide.co.uk>
2022-02-07 16:35:26 +0000

    Merged PR 484: Fix last four binding errors

---
+++ src/Chemistry/Chem4Word.ACME/Utils/BindingProxy.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------------------------
+//  Copyright (c) 2022, The .NET Foundation.
+//  This software is released under the Apache License, Version 2.0.
+//  The license and further copyright text can be found in the file LICENSE.md
+//  at the root directory of the distribution.
+// ---------------------------------------------------------------------------
+
+using System.Windows;
+
+namespace Chem4Word.ACME.Utils
+{
+    /// <summary>
+    /// Utility class to handle data binding when an element is 
+    /// not in the visual tree of the parent with the data context
+    /// </summary>
+    public class BindingProxy : Freezable
+    {
+        #region Overrides of Freezable
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        #endregion
+
+        public object Data
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+    }
+}
